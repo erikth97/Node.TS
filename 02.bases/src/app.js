4bbcfbf058853bdce8af606c183e19d7ec7ccf46
erikth97 <erik.tamayohe@uanl.edu.mx>
2024-05-04 17:31:38 -0600

    Callback

--- 02.bases/src/app.js
+++ 02.bases/src/app.js
@@ -1,4 +1,16 @@
+// const { emailTemplate } =  require('./js-fundation/01-template');
+// require('./js-fundation/02-destructuring');
+const { getUserById } = require('./js-fundation/03-callbacks');
 
-const { emailTemplate } =  require('./js-fundation/01-template');
-require('./js-fundation/02-destructuring');
 //console.log(emailTemplate);
+
+
+const id = 1;
+
+getUserById(id, function(error, user) {
+    if(error) {
+        throw new Error(error);
+    }
+
+    console.log(user);
+});
