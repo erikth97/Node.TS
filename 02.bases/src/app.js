9b0e4337b20b938bf352c4a5719f450f64d4c50e
erikth97 <erik.tamayohe@uanl.edu.mx>
2024-05-04 17:41:28 -0600

    arrow functions

--- 02.bases/src/app.js
+++ 02.bases/src/app.js
@@ -1,13 +1,14 @@
 // const { emailTemplate } =  require('./js-fundation/01-template');
 // require('./js-fundation/02-destructuring');
-const { getUserById } = require('./js-fundation/03-callbacks');
+// const { getUserById } = require('./js-fundation/03-callbacks');
+const { getUserById } = require('./js-fundation/04-arrow');
 
 //console.log(emailTemplate);
 
 
 const id = 1;
 
-getUserById(id, function(error, user) {
+getUserById(id, (error, user) => {
     if(error) {
         throw new Error(error);
     }
---
+++ 02.bases/src/js-fundation/04-arrow.js
@@ -0,0 +1,29 @@
+const users = [
+    {
+        id: 1,
+        name: 'Jhon Doe',
+    },
+    {
+        id: 2,
+        name: 'Jane Doe',
+    }
+];
+
+
+const getUserById = (id, callback) => {
+
+    const user = users.find( (user) => {
+        return user.id === id;
+    });
+
+    if(!user) {
+        return callback(`User not found with id ${id}`);
+    }
+
+    return callback(null, user);
+}
+
+
+module.exports = {
+    getUserById,
+}
