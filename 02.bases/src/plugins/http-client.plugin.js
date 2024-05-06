62d0633c7a114948e237ae62a20aeb5aabe41b5b
erikth97 <erik.tamayohe@uanl.edu.mx>
2024-05-06 12:27:32 -0600

    add Axios

--- 02.bases/src/plugins/http-client.plugin.js
+++ 02.bases/src/plugins/http-client.plugin.js
@@ -1,10 +1,13 @@
+const axios = require('axios');
 
 
 const httpClientPlugin = {
 
     get: async(url) => {
-        const resp = await fetch(url);
-        return await resp.json();
+        const {data} = await axios.get(url);
+        return data;
+        // const resp = await fetch(url);
+        // return await resp.json();
     },
 
     post: async(utl, body) => {},
