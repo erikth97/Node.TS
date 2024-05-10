58970fba372501fbc6bce68f3f36a390b26486b4
erikth97 <erik.tamayohe@uanl.edu.mx>
2024-05-10 12:58:31 -0600

    Arrange, Act, Assert | Testing in Jest

--- 02.bases/tests/app.test.ts
+++ 02.bases/tests/app.test.ts
@@ -1,12 +1,23 @@
 
-
+// A A A
 
 describe ('App', () => {
 
-    test('should be true', () => {
+    test('should be 30', () => {
 
-        expect(true).toBe(true);
+        // 1. ARRANGE
+        const num1 = 10;
+        const num2 = 20;
+
+        // 2. ACT
+        const result = num1 + num2;
+
+        // 3. ASSERT
+        expect( result ).toBe(30);
+
+
+
 
     })
-    
+
 })
