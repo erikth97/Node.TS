7570d5434e437461aa96575a8afcc7be27927f19
erikth97 <erik.tamayohe@uanl.edu.mx>
2024-05-16 20:32:32 -0600

    get uuid adapter testing

--- 02.bases/src/plugins/get-age.plugin.ts
+++ 02.bases/src/plugins/get-age.plugin.ts
@@ -4,8 +4,8 @@
 
 export const getAge = (birthdate:string) => {
 
-  console.log({currentYear: new Date().getFullYear() });
+  // console.log({currentYear: new Date().getFullYear() });
 
-  
+
   return new Date().getFullYear() - new Date(birthdate).getFullYear();
 }
