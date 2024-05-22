fb91f1bb26d57dca3f8003acf69a416770fa2c0d
erikth97 <erik.tamayohe@uanl.edu.mx>
2024-05-22 00:05:49 -0600

    create build prod

--- 02.bases/tests/plugins/http-client.plugin.test.ts
+++ 02.bases/tests/plugins/http-client.plugin.test.ts
@@ -1,26 +1,30 @@
-import { title } from "process";
 import { httpClientPlugin } from "../../src/plugins/http-client.plugin";
+
 
 describe('plugins/http-client.plugin.ts', () => {
 
-    test('httpClientPlugin.get() should return a string', async () => {
+  test('httpClientPlugin.get() should return a string', async () => {
 
-        const data = await httpClientPlugin.get('https://jsonplaceholder.typicode.com/todos/1');
+    const data = await httpClientPlugin.get('https://jsonplaceholder.typicode.com/todos/1');
 
-        expect(data).toEqual({
-            userID: 1,
-            id: 1,
-            title: "delectus aut autem",
-            completed: expect.any(Boolean)
-        });
-
+    expect(data).toEqual({
+      userId: 1,
+      id: 1,
+      title: "delectus aut autem",
+      completed: expect.any(Boolean)
     });
 
-    test('httpClientPlugin should have POST, PUT and delete methods', () => {
+  });
 
-        expect( typeof httpClientPlugin.post ).toBe('function');
-        expect( typeof httpClientPlugin.delete ).toBe('function');
-        expect( typeof httpClientPlugin.put ).toBe('function');
-        expect( typeof httpClientPlugin.get ).toBe('function');
-    })
-})
+  test('httpClientPlugin should have POST, PUT and Delete methods', () => {
+
+    expect( typeof httpClientPlugin.post ).toBe('function');
+    expect( typeof httpClientPlugin.delete ).toBe('function');
+    expect( typeof httpClientPlugin.put ).toBe('function');
+    expect( typeof httpClientPlugin.get ).toBe('function');
+
+
+  });
+
+
+});
