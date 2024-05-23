86a141781b9ce4199e78ae4d5228f03181914af8
erikth97 <erik.tamayohe@uanl.edu.mx>
2024-05-22 21:02:19 -0600

    Argv Argument Values

--- 04.multiplication/src/app.ts
+++ 04.multiplication/src/app.ts
@@ -1,24 +1,8 @@
-import fs from 'fs';
+
+console.log(process.argv);
+
+const [tsnode, app, ...args] = process.argv;
 
 
-let outputMessage = '';
-const base = 5;
-const headerMessage = `
-==============================
-    Tabla del ${ base }
-==============================\n
-`;
 
-for( let i = 1; i <=10; i++) {
-    outputMessage += `${base} x ${ i } = ${ base * i }\n`;
-}
-
-outputMessage = headerMessage + outputMessage;
-console.log(outputMessage);
-
-const outputPath = `outputs`;
-
-
-fs.mkdirSync(outputPath, { recursive: true});
-fs.writeFileSync(`${ outputPath }/tabla-${ base }.txt`, outputMessage);
-console.log('File Created');
+console.log(args);
