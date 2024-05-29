5780d5ca66f19deffd9a7bbc20180843ff4c74da
erikth97 <erik.tamayohe@uanl.edu.mx>
2024-05-28 23:25:35 -0600

    new service

--- 04.multiplication/src/presentation/server-app.ts
+++ 04.multiplication/src/presentation/server-app.ts
@@ -2,35 +2,35 @@ import { CreateTable } from "../domain/use-cases/create-table.use-case";
 import { SaveFile } from "../domain/use-cases/save-file.use-case";
 
 interface RunOptions {
-  base           : number;
-  limit          : number;
-  showTable      : boolean;
-  fileDestination: string;
-  fileName       : string;
+    base           : number;
+    limit          : number;
+    showTable      : boolean;
+    fileDestination: string;
+    fileName       : string;
 }
 
 
 export class ServerApp {
 
 
-  static run({ base, limit, showTable, fileDestination, fileName }: RunOptions) {
+static run({ base, limit, showTable, fileDestination, fileName }: RunOptions) {
     console.log('Server running...');
     
     const table = new CreateTable().execute({ base, limit });
     
     const wasCreated =  new SaveFile()
-      .execute({ 
+    .execute({ 
         fileContent: table, 
         fileDestination: fileDestination,
         fileName: fileName,
-      });
+    });
 
     if( showTable ) console.log(table);
 
     ( wasCreated )
-      ? console.log('File created!')
-      : console.error('File not created!');
+    ? console.log('File created!')
+    : console.error('File not created!');
 
-  }
+}
 
 }
