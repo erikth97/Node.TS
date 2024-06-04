336e9649e3db143d41f5a05654a1bddc45f39ab7
erikth97 <erik.tamayohe@uanl.edu.mx>
2024-06-03 19:12:25 -0600

    Test: Spy On + Mock implementation

--- 04.multiplication/src/domain/use-cases/save-file.use-case.test.ts
+++ 04.multiplication/src/domain/use-cases/save-file.use-case.test.ts
@@ -3,7 +3,7 @@ import { SaveFile } from './save-file.use-case';
 
 
 
-describe('SaveFileUseCase', () => {  
+describe('SaveFileUseCase', () => {
 
   const customOptions = {
     fileContent: 'custom content',
@@ -13,17 +13,21 @@ describe('SaveFileUseCase', () => {
 
   const customFilePath = `${customOptions.fileDestination}/${customOptions.fileName}.txt`;
 
+  // beforeEach(() => {
+  //   jest.clearAllMocks();
+  // });
+
 
   afterEach(() => {
-    const outputFolderExsist = fs.existsSync('outputs');
-    if (outputFolderExsist) fs.rmSync('outputs', {recursive: true});
-
+    const outputFolderExists = fs.existsSync('outputs');
+    if ( outputFolderExists ) fs.rmSync('outputs', { recursive: true });
+      
     const customOutputFolderExists = fs.existsSync(customOptions.fileDestination);
     if ( customOutputFolderExists ) fs.rmSync(customOptions.fileDestination, { recursive: true });
+    
+  });
 
-  })
-
-  test('Should save file with default values', () => {
+  test('should save file with default values', () => {
 
     const saveFile = new SaveFile();
     const filePath = 'outputs/table.txt';
@@ -32,15 +36,61 @@ describe('SaveFileUseCase', () => {
     }
 
     const result = saveFile.execute(options);
-    const fileExsist = fs.existsSync(filePath);
-    const fileContent = fs.readFileSync(filePath, { encoding: 'utf-8'});
-  
-    expect(result).toBe(true);
-    expect(fileExsist).toBe(true);
-    expect(fileContent).toBe(options.fileContent)
-
+    const fileExists = fs.existsSync(filePath); // ojo
+    const fileContent = fs.readFileSync(filePath, { encoding: 'utf-8' });
+    
+    expect( result ).toBe( true );
+    expect( fileExists ).toBe( true );
+    expect( fileContent ).toBe( options.fileContent );
+    
 
   });
 
 
-})
+  test('should save file with custom values', () => {
+
+    const saveFile = new SaveFile();
+    
+    const result = saveFile.execute(customOptions);
+    const fileExists = fs.existsSync(customFilePath);
+    const fileContent = fs.readFileSync(customFilePath, { encoding: 'utf-8' });
+    
+    expect( result ).toBe( true );
+    expect( fileExists ).toBe( true );
+    expect( fileContent ).toBe( customOptions.fileContent );
+
+  });
+
+  test('should return false if directory could not be created', () => {
+
+    const saveFile = new SaveFile();
+    const mkdirSpy = jest.spyOn(fs, 'mkdirSync').mockImplementation(
+      () => { throw new Error('This is a custom error message from testing'); }
+    );
+    
+    const result = saveFile.execute(customOptions);
+
+    expect( result ).toBe( false );
+
+    mkdirSpy.mockRestore();
+
+  });
+
+
+  test('should return false if file could not be created', () => {
+
+    const saveFile = new SaveFile();
+    const writeFileSpy = jest.spyOn(fs, 'writeFileSync').mockImplementation(
+      () => { throw new Error('This is a custom writing error message'); }
+    );
+    
+    const result = saveFile.execute({ fileContent: 'Hola' });
+
+    expect( result ).toBe( false );
+
+    writeFileSpy.mockRestore();
+  });
+
+
+
+});
